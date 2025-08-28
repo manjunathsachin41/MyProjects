6894228d420f55d4cef26c69c992e4e4352abeca
Manju L <manjunathsachin41@gmail.com>
2025-08-28 20:37:31 +0530

    adding custom properties

--- Commerce/src/main/java/com/example/Commerce/Controller/LoginController.java
+++ Commerce/src/main/java/com/example/Commerce/Controller/LoginController.java
@@ -1,14 +1,35 @@
 package com.example.Commerce.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PropertySource("classpath:mycustom.properties")
 public class LoginController {
 
-	@RequestMapping("/login")
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+
+	@Value("${custom.application.name}")
+	private String customAppName;
+
+	// http://localhost:8080/log
+	@RequestMapping("/log")
 	public String login() {
-		return "Logged in Succesfully!!!";
+		return "Logged in Succesfully to " + customAppName;
 	}
-	
+
+	// http://localhost:8080/login?username=admin&pwd=admin123 
+	@GetMapping("/login")
+	public String loginWithValue(@RequestParam("username") String user, @RequestParam("pwd") String pwd) {
+		String message = "Username: " + user + " Password: " + pwd + " logged in Succesfully";
+		LOGGER.info(message);
+		return message;
+	}
+
 }
