823bda69d3c60ada19f26847ea95d6e2b5fa709d
Manju L <manjunathsachin41@gmail.com>
2025-10-24 20:08:48 +0530

    Refactor

--- DemoProjects/src/com/examples/stream/CountOccurrencesOfFruits.java
+++ DemoProjects/src/com/examples/stream/CountOccurrencesOfFruits.java
@@ -2,11 +2,10 @@ package com.examples.stream;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class CountOccurrences {
+public class CountOccurrencesOfFruits {
 
 	public static void main(String[] args) {
 		List<String> words = List.of("apple", "bat", "apple", "elephant", "apple", "bat");
@@ -17,8 +16,8 @@ public class CountOccurrences {
 		resultMap.forEach((word, count) -> System.out.println( word + " : " + count));
 		
 		System.out.println("===Loop using entrySet====");
-		Set<Entry<String,Long>> entrySet = resultMap.entrySet();
-		for(Entry<String,Long> entry :entrySet) {
+		Set<Map.Entry<String,Long>> entrySet = resultMap.entrySet();
+		for(Map.Entry<String,Long> entry :entrySet) {
 			System.out.println("Word: " + entry.getKey() + " -> Count: " + entry.getValue());
 		}
 
