c101f47c2040599744552c2a4864be94e400c7c9
Manju L <manjunathsachin41@gmail.com>
2025-11-08 00:55:08 +0530

    Url Shortener

--- urlshortening/src/main/java/com/example/urlshortening/controller/UrlController.java
+++ urlshortening/src/main/java/com/example/urlshortening/controller/UrlController.java
@@ -1,6 +1,9 @@
 package com.example.urlshortening.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +14,18 @@ import com.example.urlshortening.service.UrlService;
 @RestController
 public class UrlController {
 
+	private static final Logger logger = LoggerFactory.getLogger(UrlController.class);
+
 	@Autowired
 	private UrlService urlService;
 
+	@Autowired
+	private Environment environment;
+
 	@PostMapping("/shorten")
 	public ResponseEntity<String> shortenUrl(@RequestParam(required = false) String longUrl) {
+		logger.info("Inside shortenUrl of " + longUrl);
+		logger.info("Port number : " + environment.getProperty("local.server.port"));
 		String shortUrl = urlService.shortenUrl(longUrl);
 		return new ResponseEntity<>(shortUrl, HttpStatus.OK);
 	}
