23f46f79acae9747dba2e223010f41e0d7d0e003
Manju L <manjunathsachin41@gmail.com>
2025-11-08 19:41:01 +0530

    api gateway implementation.

---
+++ api-gateway/src/main/java/com/example/api_gateway/filter/AuthenticationFilter.java
@@ -0,0 +1,62 @@
+package com.example.api_gateway.filter;
+
+import com.example.api_gateway.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+	@Autowired
+	private RouteValidator validator;
+
+	@Autowired
+	private RestTemplate template;
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	public AuthenticationFilter() {
+		super(Config.class);
+	}
+
+	@Override
+	public GatewayFilter apply(Config config) {
+		return ((exchange, chain) -> {
+			if (validator.isSecured.test(exchange.getRequest())) {
+				// checking header contains token or not
+				if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+					throw new RuntimeException("Missing authorization header");
+				}
+
+				// Get token from header
+				String authToken = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+				if (authToken != null && authToken.startsWith("Bearer ")) {
+					authToken = authToken.substring(7);
+				}
+				try {
+					/* REST call to AUTH service and validate token.
+					 * Only when token is validated, the request will be transfered to actual microservice. 
+					 */
+					//template.getForObject("http://IDENTITY-SERVICE/auth/validate?token=" + authHeader, String.class);
+
+					// Validating token in api gateway itself to avoid rest call to auth service.
+					// jwtUtil.validateToken(authHeader);
+
+				} catch (Exception e) {
+					System.out.println("Failed: "+e.getStackTrace());
+					throw new RuntimeException("Un authorized access to application" + e.getMessage());
+				}
+			}
+			return chain.filter(exchange);
+		});
+	}
+
+	public static class Config {
+
+	}
+}
